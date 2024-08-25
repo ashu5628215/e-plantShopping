2a4e7ecd18086ccfd1e5dc28e61afc055559671f
ashu5628215 <mnnitashu5628215@gmail.com>
2024-08-25 03:05:57 -0400

    Fix continue

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -249,7 +249,7 @@ const handlePlantsClick = (e) => {
 };
 
    const handleContinueShopping = (e) => {
-    e.preventDefault();
+    //e.preventDefault();
     setShowCart(false);
   };
 
@@ -265,6 +265,24 @@ const handlePlantsClick = (e) => {
         }));
     }
   };
+
+ const handleRemoveFromCart = (removedItem) => {
+    setAddedToCart((prevState) => ({
+       ...prevState,
+       [removedItem.name]: false,
+    })); 
+}; 
+
+  const totalSelectedProduct = () => {
+    let totalSelected = 0;
+    cartItems.items.forEach((item) => {
+      totalSelected += item.quantity;
+    });
+
+    return totalSelected;
+  };
+
+  const totalSelected = totalSelectedProduct();
 
     return (
         <div>
@@ -283,7 +301,7 @@ const handlePlantsClick = (e) => {
             </div>
             <div style={styleObjUl}>
                 <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg><span className="cart_quantity_count">{totalSelected}</span></h1></a></div>
             </div>
         </div>
         {!showCart? (
@@ -297,10 +315,9 @@ const handlePlantsClick = (e) => {
                                 <img className="product-image" src={plant.image} alt={plant.name} />
                                 <h3 className="product-title">{plant.name}</h3>
                                 <p className="product-price">{plant.cost}</p>
-                                <button className={`product-button ${plant.added ? 'product-button--added-to-cart' : ''}`}
-                                    onClick={() => handleAddToCart(typeIndex, plantIndex)}
-                                    disabled={addedToCart[plant.name]}>
-                                    {addedToCart[plant.name] ? 'Added to Cart' : 'Add to Cart'}
+                                <button className={cartItems.items.filter((prod) => prod.name == plant.name).length == 0 ? "product-button" : "product-button added-to-cart"}
+                                    onClick={() => handleAddToCart(typeIndex, plantIndex)}>
+                                    {cartItems.items.filter((prod) => prod.name == plant.name).length == 0 ? "Add to Cart" : "Added to Cart"}{" "}
                                 </button>
                             </div>
                         ))}
@@ -310,7 +327,7 @@ const handlePlantsClick = (e) => {
             <br></br>
         </div>
  ) :  (
-    <CartItem onContinueShopping={handleContinueShopping}/>
+    <CartItem  onContinueShopping={handleContinueShopping} />
 )}
     </div>
     );
